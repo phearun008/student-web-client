485377850ef09fa56f538fe90b7941a3c9f9ee9a
phearun008 <PC1@192.168.178.71>
2016-08-20 11:13:07 +0700

    change

--- pp-student-web-client/src/main/java/student/service/UserDetailServiceImpl.java
+++ pp-student-web-client/src/main/java/student/service/UserDetailServiceImpl.java
@@ -1,4 +1,4 @@
-package student.entity.service;
+package student.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
